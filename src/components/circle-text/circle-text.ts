376898af01599984fadfe4ca758b6baef23662f2
duwei <905571995@qq.com>
2018-09-08 22:42:17 +0800

    feat: 添加circle-text组件

---
+++ src/components/circle-text/circle-text.ts
@@ -0,0 +1,19 @@
+Component({
+  options: {
+    addGlobalClass: true,
+  },
+  properties: {
+    text: {
+      type: String,
+      value: ''
+    },
+    active: {
+      type: Boolean,
+      value: false
+    },
+    color: {
+      type: String,
+      value: '#fff'
+    }
+  }
+});
