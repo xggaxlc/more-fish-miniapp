d28b88508e0e8e9e97757a437b10cdfa01a23ac6
duwei <905571995@qq.com>
2018-09-08 22:02:36 +0800

    feat: 记账页面还原

--- src/pages/bill-form/bill-form.ts
+++ src/pages/bill-form/bill-form.ts
@@ -1,5 +1,7 @@
 import { observer, checkCurrentBook, BillListStore, userStore, BillStore, BudgetListStore } from '@store';
 import { autoLoading, goBack, showToast } from '@utils';
+import * as dayjs from 'dayjs';
+import get from 'lodash-es/get';
 
 observer({
   get props() {
@@ -18,10 +20,25 @@ observer({
       });
   },
 
+  data: {
+    time: dayjs().format('YYYY-MM-DD'),
+    budgetIndex: -1
+  },
+
   async onLoad() {
-    const billStore = this.props.billStore;
+    const { billStore, budgetListStore } = this.props;
+    await budgetListStore.fetchData();
     if (billStore) {
       await billStore.fetchData();
+
+      const budgetId = get(billStore, 'data.budget._id');
+      let index = -1;
+      budgetListStore.data.forEach((item, i) => {
+        if (get(item, 'data._id') === budgetId) {
+          index = i;
+        }
+      });
+      this.setData({ budgetIndex: index, time: dayjs(billStore.data.time).format('YYYY-MM-DD') });
     }
   },
 
@@ -38,5 +55,14 @@ observer({
     }
     await showToast('保存成功');
     goBack();
+  },
+
+  handleTimeChange(e) {
+    this.setData({ time: e.detail.value });
+  },
+
+  handleSelectBudget(e) {
+    const index = e.currentTarget.dataset.index;
+    this.setData({ budgetIndex: index });
   }
 });
