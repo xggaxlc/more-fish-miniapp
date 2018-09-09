3920e819805f63c49b660c9208de2cd0654ffdf2
duwei <905571995@qq.com>
2018-09-09 09:51:06 +0800

    feat: 添加账本详情页面

--- src/pages/book-detail/book-detail.ts
+++ src/pages/book-detail/book-detail.ts
@@ -1,5 +1,31 @@
-import { observer } from '@store';
+import { showConfirmModal } from '@utils';
+import { observer, BookStore } from '@store';
 
 observer({
-  props: {}
+  get props() {
+    return {
+      bookStore: BookStore.findOrCreate(this.options.id)
+    }
+  },
+
+  onLoad() {
+    return this.props.bookStore.fetchData();
+  },
+
+  async handleDelete() {
+    await showConfirmModal('账本');
+    await this.props.bookStore.deleteBook();
+    wx.reLaunch({
+      url: '/pages/book-list/book-list'
+    });
+  },
+
+  async handleExit() {
+    await showConfirmModal('账本');
+    await this.props.bookStore.exitBook();
+    wx.reLaunch({
+      url: '/pages/book-list/book-list'
+    });
+  }
+
 });
