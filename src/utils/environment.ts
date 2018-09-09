24824739e11355fb74c61fdf816de6825bd4fd1c
duwei <905571995@qq.com>
2018-09-09 12:40:16 +0800

    feat: env 相关设置

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -3,13 +3,12 @@ const { ip, NODE_ENV = 'development', PORT = 4000 } = process.env;
 const environmentConfig = {
   development: {
     apiOrigin: `http://${ip}:${PORT}`,
-  },
-  staging: {
-    apiOrigin: `http://${ip}:${PORT}`,
+    clouldeEnv: 'develop-6e2138'
   },
   production: {
     apiOrigin: 'http://45.76.13.154:7000',
+    clouldeEnv: 'production-d1725e'
   },
 }
 
-export const { apiOrigin } = environmentConfig[NODE_ENV]
+export const { apiOrigin, clouldeEnv } = environmentConfig[NODE_ENV]
