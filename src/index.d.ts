fa96952950b8fff521f57a6f6675435e988d1982
duwei <905571995@qq.com>
2018-09-12 17:51:42 +0800

    feat: 添加weapp.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,4 @@
-declare var App: any;
-declare var Page: any;
-declare var Component: any;
 declare var IS_PRODUCTION: boolean;
-declare var getApp: any;
 declare var wx: any;
-declare var getCurrentPages: any;
 declare var process;
 declare var require;
