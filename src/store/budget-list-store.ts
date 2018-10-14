bf6f2c64cd906a3826c615e17ebf1d25ead615d8
duwei <905571995@qq.com>
2018-10-14 12:25:13 +0800

    refactor: 预算列表样式调整

--- src/store/budget-list-store.ts
+++ src/store/budget-list-store.ts
@@ -4,13 +4,34 @@ import get from 'lodash-es/get';
 import sumBy from 'lodash-es/sumBy';
 import { BudgetStore } from './budget-store';
 import { fetch } from '@utils';
+import * as dayjs from 'dayjs';
 
 class BudgetListStore extends WebAPIStore {
   @observable data = [];
 
+  @observable form = {
+    year: dayjs().year(),
+    month: dayjs().month()
+  }
+
+  getParams() {
+    const { year, month } = this.form;
+    const $time = dayjs().set('year', year).set('month', month);
+
+    return {
+      start_at: $time.startOf('month').toISOString(),
+      end_at: $time.endOf('month').toISOString()
+    }
+  }
+
+  @action
+  updateForm(body) {
+    Object.assign(this.form, body);
+  }
+
   @fetchAction.merge
   async fetchData(params = {}) {
-    const { data: originData } = await fetch('/books/$$bookId/budgets', { data: params });
+    const { data: originData } = await fetch('/books/$$bookId/budgets', { data: { ...this.getParams(), ...params } });
     const data = originData.map(item => BudgetStore.createOrUpdate(item._id, { data: item }));
     return { data };
   }
@@ -36,12 +57,18 @@ class BudgetListStore extends WebAPIStore {
     return sumBy(this.data, item => item.data.amount).toFixed(2);
   }
 
-
   @asyncAction
   async* create(data) {
     const { data: { _id } } = yield fetch('/books/$$bookId/budgets', { method: 'POST', data });
     this.fetchData();
     return _id;
+  }
+
+  @computed
+  get currentDate() {
+    const { year, month } = this.form;
+    const $time = dayjs().set('year', year).set('month', month);
+    return $time.format('YYYY-MM');
   }
 }
 
